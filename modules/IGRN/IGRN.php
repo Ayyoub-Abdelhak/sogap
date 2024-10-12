4bccf6cc45163d6d9c078ba0cbafc4615df236f7
Ayyoub Abdelhak <abdelhak.ayyoub@gmail.com>
2024-10-12 01:53:56 +0100

    no message

--- modules/IGRN/IGRN.php
+++ modules/IGRN/IGRN.php
@@ -12,7 +12,7 @@ class IGRN extends Vtiger_CRMEntity
 {
 	public $table_name = 'u_yf_igrn';
 	public $table_index = 'igrnid';
-	protected $lockFields = ['igrn_status' => ['PLL_ACCEPTED']];
+	// protected $lockFields = ['igrn_status' => ['PLL_ACCEPTED']];
 
 	/**
 	 * Mandatory table for supporting custom fields.
