015871c4f2d0f95fc03d5fa022002ccdc4fab17b
Ayyoub Abdelhak <abdelhak.ayyoub@gmail.com>
2024-10-15 23:22:33 +0100

    no message

--- app/TextParser/ProductsTableDevisVersion.php
+++ app/TextParser/ProductsTableDevisVersion.php
@@ -85,10 +85,14 @@ class ProductsTableDevisVersion extends Base
 				$columnName = $fieldModel->getColumnName();
 				$typeName = $fieldModel->getType();
 				$headerName = \App\Language::translate($fieldModel->get('label'), $this->textParser->moduleName);
-                if($typeName == 'Name') $headerName = 'Désignation';
-				if($typeName == 'Value') $headerName = 'Un.';
-				if($typeName == 'UnitPrice') $headerName = 'Prix U. HT';
-				if($typeName == 'TotalPrice') $headerName = 'Prix Total HT';
+				if ($typeName == 'Name')
+					$headerName = 'Désignation';
+				if ($typeName == 'Value')
+					$headerName = 'Un.';
+				if ($typeName == 'UnitPrice')
+					$headerName = 'Prix U. HT';
+				if ($typeName == 'TotalPrice')
+					$headerName = 'Prix Total HT';
 				if (!$fieldModel->isVisible()) {
 					continue;
 				}
@@ -104,15 +108,15 @@ class ProductsTableDevisVersion extends Base
 			foreach ($inventoryRows as $inventoryRow) {
 				++$counter;
 
-                $displayType = $inventoryRow['picklist1'];
-                $displayStyle = '';
-                if ($displayType == 'T') {
-                    $displayStyle = 'font-weight:bold;font-size:8.5px;';
-                } elseif ($displayType == 'ST') {
-                    $displayStyle = 'font-weight:bold;';
-                } else {
-                    $displayStyle = 'font-weight:normal;';
-                }
+				$displayType = $inventoryRow['picklist1'];
+				$displayStyle = '';
+				if ($displayType == 'T') {
+					$displayStyle = 'font-weight:bold;font-size:8.5px;';
+				} elseif ($displayType == 'ST') {
+					$displayStyle = 'font-weight:bold;';
+				} else {
+					$displayStyle = 'font-weight:normal;';
+				}
 
 				$html .= '<tr class="row-' . $counter . '">';
 				foreach ($groupModels as $fieldModel) {
@@ -128,6 +132,9 @@ class ProductsTableDevisVersion extends Base
 						$itemValue = $inventoryRow[$columnName];
 						if ('Name' === $typeName) {
 							$fieldStyle = $bodyStyle . 'width: 300px !important;' . $displayStyle;
+							if ($discount > 0) {
+								$fieldStyle = $bodyStyle . 'width: 250px !important;' . $displayStyle;
+							}
 							$fieldValue = $fieldModel->getDisplayValue($itemValue, $inventoryRow, true) === 'Produit non trouvé' ? '' : $fieldModel->getDisplayValue($itemValue, $inventoryRow, true);
 							$lines--;
 							foreach ($inventory->getFieldsByType('Comment') as $commentField) {
@@ -143,7 +150,7 @@ class ProductsTableDevisVersion extends Base
 							$fieldValue = $fieldModel->getDisplayValue($itemValue, $inventoryRow, true);
 						}
 						$fieldValue = in_array($fieldValue, ['0', '0.00 DH', '0 DH', 'nan']) ? '' : $fieldValue;
-						$fieldValue = str_replace("DH","",$fieldValue);
+						$fieldValue = str_replace("DH", "", $fieldValue);
 						$html .= "<td class=\"col-type-{$typeName}\" style=\"{$fieldStyle}\">" . $fieldValue . '</td>';
 					}
 				}
@@ -155,40 +162,52 @@ class ProductsTableDevisVersion extends Base
 			$html .= '</tbody><tfoot><tr>';
 
 			foreach ($groupModels as $fieldModel) {
-                $headerStyle = 'border:1px solid black;border-top:0px;border-bottom:0px;padding:0px;';
-                $html .= "<th style=\"{$headerStyle}\">";
-                $html .= '</th>';
-            }
-            $html .= '</tr></tfoot></table>';
+				$headerStyle = 'border:1px solid black;border-top:0px;border-bottom:0px;padding:0px;';
+				$html .= "<th style=\"{$headerStyle}\">";
+				$html .= '</th>';
+			}
+			$html .= '</tr></tfoot></table>';
 
-            $html .= '
-                <div style="padding: 0px 0px 0px 221px">
-                    <table style="width:100%;font-size:8px;margin-top:15px;border:1px solid black;font-weight:bold;">
-                        <tr>
-                            <td style="width:75%;text-align:center;border-right-style:solid;border-right-width:1px;border-bottom-style:solid;border-bottom-width:1px;">
-                                TOTAL HT
-                            </td>
-                            <td style="width: 25%;text-align:center;border-bottom-style:solid;border-bottom-width:1px;">' . \CurrencyField::convertToUserFormat($ht, null, true) . '</td>
-                        </tr>
-                        <tr>
-                            <td style="width:75%;text-align:center;border-right-style:solid;border-right-width:1px;border-bottom-style:solid;border-bottom-width:1px;">
-                                TVA 20%
-                            </td>
-                            <td style="width: 25%;text-align:center;border-bottom-style:solid;border-bottom-width:1px;">' . \CurrencyField::convertToUserFormat($ht * 0.2, null, true) . '</td>
-                        </tr>
-                        <tr>
-                            <td style="width:75%;text-align:center;border-right-style:solid;border-right-width:1px;">
-                                TOTAL TTC
-                            </td>
-                            <td style="width: 25%;text-align:center;">' . \CurrencyField::convertToUserFormat($ttc, null, true) . '</td>
-                        </tr>
-                    </table>
-                </div>
-            ';
+			$html .= '
+			<div style="padding: 0px 0px 0px 221px">
+				<table style="width:100%;font-size:8px;margin-top:15px;border:1px solid black;font-weight:bold;">
+					<tr>
+						<td style="width:75%;text-align:center;border-right-style:solid;border-right-width:1px;border-bottom-style:solid;border-bottom-width:1px;">
+							TOTAL HT
+						</td>
+						<td style="width: 25%;text-align:center;border-bottom-style:solid;border-bottom-width:1px;">' . \CurrencyField::convertToUserFormat($ht, null, true) . '</td>
+					</tr>';
+			if ($tax > 0) {
+				$html .= '
+				<tr>
+					<td style="width:75%;text-align:center;border-right-style:solid;border-right-width:1px;border-bottom-style:solid;border-bottom-width:1px;">
+						TVA 20%
+					</td>
+					<td style="width: 25%;text-align:center;border-bottom-style:solid;border-bottom-width:1px;">' . \CurrencyField::convertToUserFormat($tax, null, true) . '</td>
+				</tr>';
+			}
+			if ($discount > 0) {
+				$html .= '
+				<tr>
+					<td style="width:75%;text-align:center;border-right-style:solid;border-right-width:1px;border-bottom-style:solid;border-bottom-width:1px;">
+						Remise
+					</td>
+					<td style="width: 25%;text-align:center;border-bottom-style:solid;border-bottom-width:1px;">' . \CurrencyField::convertToUserFormat($discount, null, true) . '</td>
+				</tr>';
+			}
+			$html .= '
+					<tr>
+						<td style="width:75%;text-align:center;border-right-style:solid;border-right-width:1px;">
+							TOTAL TTC
+						</td>
+						<td style="width: 25%;text-align:center;">' . \CurrencyField::convertToUserFormat($ttc, null, true) . '</td>
+					</tr>
+				</table>
+			</div>';
 
-            $currency = \Vtiger_Util_Helper::is_decimal($ttc) ? '' : ' DIRHAMS';
-            $html .= '<div style="font-size:9px;margin-top:20px;"><b>ARRÊTÉ LE PRÉSENT DEVIS À LA SOMME DE : ' . \Vtiger_Util_Helper::int2str($ttc) . $currency . ' TTC</b></div>';
-			if($this->textParser->recordModel->get('validite') != '') {
+			$currency = \Vtiger_Util_Helper::is_decimal($ttc) ? '' : ' DIRHAMS';
+			$html .= '<div style="font-size:9px;margin-top:20px;"><b>ARRÊTÉ LE PRÉSENT DEVIS À LA SOMME DE : ' . \Vtiger_Util_Helper::int2str($ttc) . $currency . ' TTC</b></div>';
+			if ($this->textParser->recordModel->get('validite') != '') {
 				$html .= '<div style="font-size:8px;margin-top:20px;"><b>Validité de l\'offre : ' . $this->textParser->recordModel->get('validite') . '</b></div>';
 			}
 		}
