9d3861799bd4f2209741eba22cebaceaffa3f63b
Ayyoub Abdelhak <abdelhak.ayyoub@gmail.com>
2024-12-09 15:32:41 +0100

    entree/sorties produits default select

---
+++ public_html/layouts/basic/modules/ISTDN/resources/Edit.js
@@ -0,0 +1,14 @@
+/* {[The file is published on the basis of YetiForce Public License 6.5 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
+"use strict";
+
+Vtiger_Edit_Js(
+  "ISTDN_Edit_Js",
+  {},
+  {
+    getRecordsListParams: function (container) {
+      var params = this._super(container);
+      params.cvId = "136";
+      return params;
+    },
+  }
+);
---
+++ public_html/layouts/basic/modules/ISTRN/resources/Edit.js
@@ -0,0 +1,12 @@
+/* {[The file is published on the basis of YetiForce Public License 6.5 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
+"use strict";
+Vtiger_Edit_Js(
+  "ISTRN_Edit_Js",
+  {},
+  {
+    getRecordsListParams: function (e) {
+      var t = this._super(e);
+      return (t.cvId = "136"), t;
+    },
+  }
+);
