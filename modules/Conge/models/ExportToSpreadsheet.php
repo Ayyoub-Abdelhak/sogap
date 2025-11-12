9525212def0d56071dea6c3a1be3d96488263377
Sogap <sogap.ma@gmail.com>
2025-11-12 16:08:17 +0100

    conge facture export calcul

--- modules/Conge/models/ExportToSpreadsheet.php
+++ modules/Conge/models/ExportToSpreadsheet.php
@@ -14,49 +14,83 @@
  */
 class Conge_ExportToSpreadsheet_Model extends Vtiger_ExportToSpreadsheet_Model
 {
-    /** {@inheritdoc} */
-    public function sanitizeValues(array $row): array
-    {
-        // Calculate the additional columns before exporting
-        $soldeFN1 = (float) ($row['soldefinnmoins1'] ?? 0);
-        $droitCp = (float) ($row['droit_cp_annee_encours'] ?? 0);
-        $soldeInitial = $soldeFN1 + $droitCp;
+	/** {@inheritdoc} */
+	public function sanitizeValues(array $row): array
+	{
+		// Check if we need to calculate any of the special fields
+		$needsCalculation = false;
+		foreach ($this->fields as $fieldModel) {
+			$fieldName = $fieldModel->getName();
+			if (in_array($fieldName, ['solde_initial', 'consommations_cp_n', 'solde_cp_n'])) {
+				$needsCalculation = true;
+				break;
+			}
+		}
+		
+		// If we need to calculate, fetch the required fields from database if not in row
+		if ($needsCalculation) {
+			$requiredFields = ['soldefinnmoins1', 'droit_cp_annee_encours', 'janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre'];
+			$missingFields = array_diff($requiredFields, array_keys($row));
+			
+			// Fetch missing fields from database
+			if (!empty($missingFields) && !empty($row['id'])) {
+				$recordData = (new \App\Db\Query())
+					->select($missingFields)
+					->from('u_yf_conge')
+					->innerJoin('vtiger_crmentity', 'u_yf_conge.congeid = vtiger_crmentity.crmid')
+					->where(['vtiger_crmentity.crmid' => $row['id'], 'vtiger_crmentity.deleted' => 0])
+					->one();
+				
+				if ($recordData) {
+					$row = array_merge($row, $recordData);
+				}
+			}
+			
+			// Calculate the additional columns
+			$soldeFN1 = (float)($row['soldefinnmoins1'] ?? 0);
+			$droitCp = (float)($row['droit_cp_annee_encours'] ?? 0);
+			$soldeInitial = $soldeFN1 + $droitCp;
+			
+			$janvier = (float)($row['janvier'] ?? 0);
+			$fevrier = (float)($row['fevrier'] ?? 0);
+			$mars = (float)($row['mars'] ?? 0);
+			$avril = (float)($row['avril'] ?? 0);
+			$mai = (float)($row['mai'] ?? 0);
+			$juin = (float)($row['juin'] ?? 0);
+			$juillet = (float)($row['juillet'] ?? 0);
+			$aout = (float)($row['aout'] ?? 0);
+			$septembre = (float)($row['septembre'] ?? 0);
+			$octobre = (float)($row['octobre'] ?? 0);
+			$novembre = (float)($row['novembre'] ?? 0);
+			$decembre = (float)($row['decembre'] ?? 0);
+			
+			$consomations = $janvier + $fevrier + $mars + $avril + $mai + $juin + $juillet + $aout + $septembre + $octobre + $novembre + $decembre;
+			$soldeCP = $soldeInitial - $consomations;
+			
+			// Override the values in the row
+			$row['solde_initial'] = $soldeInitial;
+			$row['consommations_cp_n'] = $consomations;
+			$row['solde_cp_n'] = $soldeCP;
+		}
+		
+		// Now process the row with the parent method
+		$this->colNo = 1;
+		foreach ($this->fields as $originalDbKey => $fieldModel) {
+			$dbKey = $originalDbKey;
+			$idKey = 'id';
+			if ($fieldModel->get('source_field_name')) {
+				$name = $fieldModel->get('source_field_name') . $fieldModel->getModuleName();
+				$idKey = $name . $idKey;
+				$dbKey = $name . $fieldModel->getName();
+			}
+			
+			// For calculated fields, check if we have them in the row with the original key
+			$value = $row[$dbKey] ?? $row[$originalDbKey] ?? '';
 
-        $janvier = (float) ($row['janvier'] ?? 0);
-        $fevrier = (float) ($row['fevrier'] ?? 0);
-        $mars = (float) ($row['mars'] ?? 0);
-        $avril = (float) ($row['avril'] ?? 0);
-        $mai = (float) ($row['mai'] ?? 0);
-        $juin = (float) ($row['juin'] ?? 0);
-        $juillet = (float) ($row['juillet'] ?? 0);
-        $aout = (float) ($row['aout'] ?? 0);
-        $septembre = (float) ($row['septembre'] ?? 0);
-        $octobre = (float) ($row['octobre'] ?? 0);
-        $novembre = (float) ($row['novembre'] ?? 0);
-        $decembre = (float) ($row['decembre'] ?? 0);
+			$this->putDataIntoSpreadsheet($fieldModel, $value, $row[$idKey] ?? 0);
+		}
+		++$this->rowNo;
 
-        $consomations = $janvier + $fevrier + $mars + $avril + $mai + $juin + $juillet + $aout + $septembre + $octobre + $novembre + $decembre;
-        $soldeCP = $soldeInitial - $consomations;
-
-        // Add calculated values to the row
-        $row['solde_initial'] = $soldeInitial;
-        $row['consommations_cp_n'] = $consomations;
-        $row['solde_cp_n'] = $soldeCP;
-
-        // Now process the row with the parent method
-        $this->colNo = 1;
-        foreach ($this->fields as $dbKey => $fieldModel) {
-            $idKey = 'id';
-            if ($fieldModel->get('source_field_name')) {
-                $name = $fieldModel->get('source_field_name') . $fieldModel->getModuleName();
-                $idKey = $name . $idKey;
-                $dbKey = $name . $fieldModel->getName();
-            }
-
-            $this->putDataIntoSpreadsheet($fieldModel, $row[$dbKey] ?? '', $row[$idKey] ?? 0);
-        }
-        ++$this->rowNo;
-
-        return [];
-    }
+		return [];
+	}
 }
--- modules/FInvoice/models/ExportToSpreadsheet.php
+++ modules/FInvoice/models/ExportToSpreadsheet.php
@@ -14,43 +14,66 @@
  */
 class FInvoice_ExportToSpreadsheet_Model extends Vtiger_ExportToSpreadsheet_Model
 {
-    /** {@inheritdoc} */
-    public function sanitizeValues(array $row): array
-    {
-        // Get the base values from the database
-        $totalHT = (float) ($row['sum_total'] ?? 0); // HT = sum_total
-        $totalTTC = (float) ($row['sum_gross'] ?? 0); // TTC = sum_gross
-        $retenueGarantiePercentage = (float) ($row['retenue_garantie'] ?? 0);
+	/** {@inheritdoc} */
+	public function sanitizeValues(array $row): array
+	{
+		// Check if sum_gross is being exported
+		$sumGrossExists = false;
+		foreach ($this->fields as $fieldModel) {
+			if ($fieldModel->getName() === 'sum_gross') {
+				$sumGrossExists = true;
+				break;
+			}
+		}
+		
+		// Only modify sum_gross if it's being exported
+		if ($sumGrossExists && isset($row['sum_gross'])) {
+			// Fetch retenue_garantie if not in row
+			if (!isset($row['retenue_garantie']) && !empty($row['id'])) {
+				$recordData = (new \App\Db\Query())
+					->select(['retenue_garantie'])
+					->from('u_yf_finvoice')
+					->innerJoin('vtiger_crmentity', 'u_yf_finvoice.finvoiceid = vtiger_crmentity.crmid')
+					->where(['vtiger_crmentity.crmid' => $row['id'], 'vtiger_crmentity.deleted' => 0])
+					->one();
+				
+				if ($recordData) {
+					$row['retenue_garantie'] = $recordData['retenue_garantie'];
+				}
+			}
+			
+			$totalTTC = (float)$row['sum_gross'];
+			$retenueGarantiePercentage = (float)($row['retenue_garantie'] ?? 0);
+			
+			// Calculate retenue de garantie amount
+			$retenueGarantie = $totalTTC * $retenueGarantiePercentage / 100;
+			
+			// Calculate total after retenue de garantie
+			$totalRGTTC = round($totalTTC - $retenueGarantie, 2);
+			
+			// Replace sum_gross with the calculated value (TTC - retenue_garantie)
+			$row['sum_gross'] = $totalRGTTC;
+		}
+		
+		// Now process the row with the parent method
+		$this->colNo = 1;
+		foreach ($this->fields as $dbKey => $fieldModel) {
+			$idKey = 'id';
+			if ($fieldModel->get('source_field_name')) {
+				$name = $fieldModel->get('source_field_name') . $fieldModel->getModuleName();
+				$idKey = $name . $idKey;
+				$dbKey = $name . $fieldModel->getName();
+			}
+			
+			// Get the actual field name to look up in the row
+			$fieldName = $fieldModel->getName();
+			$value = $row[$fieldName] ?? ($row[$dbKey] ?? '');
+			$id = $row[$idKey] ?? 0;
 
-        // Calculate retenue de garantie amount
-        $retenueGarantie = $totalTTC * $retenueGarantiePercentage / 100;
+			$this->putDataIntoSpreadsheet($fieldModel, $value, $id);
+		}
+		++$this->rowNo;
 
-        // Calculate total after retenue de garantie
-        $totalRGTTC = round($totalTTC - $retenueGarantie, 2);
-
-        // Calculate TVA (TTC - HT)
-        $totalTVA = $totalTTC - $totalHT;
-
-        // Add calculated values to the row
-        // These fields should match the field names in your FInvoice module
-        $row['retenue_garantie_amount'] = $retenueGarantie;
-        $row['total_rg_ttc'] = $totalRGTTC;
-        $row['total_tva'] = $totalTVA;
-
-        // Now process the row with the parent method
-        $this->colNo = 1;
-        foreach ($this->fields as $dbKey => $fieldModel) {
-            $idKey = 'id';
-            if ($fieldModel->get('source_field_name')) {
-                $name = $fieldModel->get('source_field_name') . $fieldModel->getModuleName();
-                $idKey = $name . $idKey;
-                $dbKey = $name . $fieldModel->getName();
-            }
-
-            $this->putDataIntoSpreadsheet($fieldModel, $row[$dbKey] ?? '', $row[$idKey] ?? 0);
-        }
-        ++$this->rowNo;
-
-        return [];
-    }
+		return [];
+	}
 }
