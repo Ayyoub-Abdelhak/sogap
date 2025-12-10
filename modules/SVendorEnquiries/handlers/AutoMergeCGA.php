90d9658950e79dbf060922c185e11e54b89cf0df
Ayyoub Abdelhak <abdelhak.ayyoub@gmail.com>
2025-12-10 15:48:15 +0100

    Watermark

--- modules/SVendorEnquiries/handlers/AutoMergeCGA.php
+++ modules/SVendorEnquiries/handlers/AutoMergeCGA.php
@@ -16,12 +16,17 @@
 class SVendorEnquiries_AutoMergeCGA_Handler
 {
 	/**
-	 * CGA PDF Template ID - Conditions Générales Achat template.
+	 * Main SVendorEnquiries template ID (with watermark).
+	 */
+	public const MAIN_TEMPLATE_ID = 38;
+
+	/**
+	 * CGA PDF Template ID - Conditions Générales Achat template (without watermark).
 	 */
 	public const CGA_TEMPLATE_ID = 55;
 
 	/**
-	 * PdfGenerateInit handler function - automatically add CGA template to merge.
+	 * PdfGenerateInit handler function - automatically add CGA template when main template is generated.
 	 *
 	 * @param App\EventHandler $eventHandler
 	 *
@@ -29,30 +34,19 @@ class SVendorEnquiries_AutoMergeCGA_Handler
 	 */
 	public function pdfGenerateInit(App\EventHandler $eventHandler)
 	{
-		// Only process if CGA template ID is configured
-		if (null === self::CGA_TEMPLATE_ID) {
-			\App\Log::warning('SVendorEnquiries Auto Merge CGA: CGA_TEMPLATE_ID not configured in handler');
-			return;
-		}
-
 		// Get current templates from the event
 		$templates = $eventHandler->getParam('templates');
 
-		// Check if CGA template is already in the list
-		if (!in_array(self::CGA_TEMPLATE_ID, $templates)) {
-			// Add CGA template to the list
-			$templates[] = self::CGA_TEMPLATE_ID;
-			$eventHandler->addParams('templates', $templates);
+		// Check if the main template (38) is being generated
+		if (in_array(self::MAIN_TEMPLATE_ID, $templates)) {
+			// Check if CGA template is not already in the list
+			if (!in_array(self::CGA_TEMPLATE_ID, $templates)) {
+				// Add CGA template to the list
+				$templates[] = self::CGA_TEMPLATE_ID;
+				$eventHandler->addParams('templates', $templates);
 
-			\App\Log::trace('SVendorEnquiries Auto Merge CGA: Added CGA template to PDF generation');
-		}
-
-		// Get the view instance to modify the request
-		$viewInstance = $eventHandler->getParam('viewInstance');
-		if ($viewInstance && method_exists($viewInstance, 'request')) {
-			// Force single PDF mode to enable auto-merge
-			// Note: This might not work depending on YetiForce version
-			// The merge will happen automatically if multiple templates are selected
+				\App\Log::trace('SVendorEnquiries Auto Merge CGA: Added CGA template (55) to merge with main template (38)');
+			}
 		}
 	}
 }
